d0c29f194dd0a4e38928a9d237aefa10f1b900c5
Alex <akh.default@gmail.com>
2022-07-27 16:21:16 +0300

    Add example with storybook

---
+++ packages/create-app/.eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: '@akh-test/eslint-config',
+};
---
+++ packages/create-app/src/index.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import { spawn } from 'child_process';
+import path from 'path';
+// import util from 'util';
+
+// const execAsync = util.promisify(exec);
+
+(async () => {
+  const child = spawn('npx', [
+    'create-next-app@latest',
+    '--example-path',
+    path.join(__dirname, './examples/default'),
+  ]);
+  console.log(path.join(__dirname, './examples/default'));
+
+  child.stdin.setEncoding('utf-8');
+  child.stdout.pipe(process.stdout);
+  process.stdin.pipe(child.stdin);
+  child.stderr.on('data', data => {
+    console.error(`stderr: ${data}`);
+  });
+})();
