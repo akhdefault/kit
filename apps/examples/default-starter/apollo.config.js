a760bcc31d4b749c974b41e9c54900054f2a4eb9
Alex <akh.default@gmail.com>
2022-07-29 14:51:56 +0300

    Cleanup

--- apps/examples/default-starter/apollo.config.js
+++ apps/examples/default-starter/apollo.config.js
@@ -1,8 +1,9 @@
 const dotenv = require('dotenv');
 const path = require('path');
 
+const envPath = path.resolve(__dirname, '.env');
 dotenv.config({
-  path: path.resolve(__dirname, '.env'),
+  path: envPath,
 });
 
 module.exports = {
